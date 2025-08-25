c96d4da800d4743ef420197381678d7f3636179c
Shothodzo <86077168+Shots-Dev@users.noreply.github.com>
2025-08-25 11:12:03 +0200

    Update Task 4.py
    Updated the code as per the kata level 2 task 1

--- Task 4.py
+++ Task 4.py
@@ -1,9 +1,21 @@
-#Task 4
+#Task 4 (kata level 1)
+
+# num = 3
+
+# def square(num):
+#     for hash in range(num):
+#         print('#'*num)
+
+# square(num)
+
+
+# Task 1 (Kata level 2)
 
 num = 3
+symbol = '@'
 
-def square(num):
+def square(num,symbol):
     for hash in range(num):
-        print('#'*num)
+        print(symbol*num)
 
-square(num)
+square(num,symbol)
